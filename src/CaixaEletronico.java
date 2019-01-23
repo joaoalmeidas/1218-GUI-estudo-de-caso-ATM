868fd7f9097db8d748765fad16cbac2f5052f924
joao <joao182cx@gmail.com>
2019-01-23 10:43:32 -0200

    Criação do contrutor da classe CaixaEletronico

--- src/CaixaEletronico.java
+++ src/CaixaEletronico.java
@@ -1,3 +1,5 @@
+import java.awt.GridLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -22,5 +24,46 @@ public class CaixaEletronico extends JFrame {
 	private JButton sacaDinheiro;
 	private JButton insereEnvelope;
 	
-	
+	public CaixaEletronico(){
+		
+		super("Caixa Eletrônio");
+		setLayout(new GridLayout(2, 1));
+		
+		tela = new JPanel(new GridLayout(3, 1));
+		teclado = new JPanel(new GridLayout(4, 3));
+		depositoESaque = new JPanel(new GridLayout(2, 1));
+		painelUsuario = new JPanel(new GridLayout(1, 2));
+		
+		add(tela);
+		add(painelUsuario);
+		
+		painelUsuario.add(teclado);
+		painelUsuario.add(depositoESaque);
+		
+		infoTela = new JTextArea("1 - 20; 2 - 200");
+		infoTela.setEditable(false);
+		labelEntradaUsuario = new JLabel("Escolha uma quantia para sacar:");
+		entradaUsuario = new JTextField(20);
+		
+		tela.add(infoTela);
+		tela.add(labelEntradaUsuario);
+		tela.add(entradaUsuario);
+		
+		tecladoNumerico = new JButton[numeros.length];
+		for(int i = 0; i < numeros.length; i++) {
+			
+			tecladoNumerico[i] = new JButton();
+			tecladoNumerico[i].setText(numeros[i]);
+			teclado.add(tecladoNumerico[i]);
+			
+		}
+		
+		sacaDinheiro = new JButton("Retire o dinheito aqui");
+		insereEnvelope = new JButton("Insira o envelope aqui");
+		
+		depositoESaque.add(sacaDinheiro);
+		depositoESaque.add(insereEnvelope);
+		
+		
+	}
 }
