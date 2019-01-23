a35c6bb08c37e6d7fcb0ee4af3edba72d21c7cd2
joao <joao182cx@gmail.com>
2019-01-23 10:46:52 -0200

    Criação da classe de teste

---
+++ src/CaixaEletronicoTeste.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class CaixaEletronicoTeste {
+
+	public static void main(String[] args) {
+		
+		CaixaEletronico caixa = new CaixaEletronico();
+		caixa.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		caixa.setSize(1000, 1000);
+		caixa.setVisible(true);
+		
+	}
+
+}
